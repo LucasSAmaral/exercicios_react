99589abe1ca17ac296efc46fe0707d1ba87366a5
Lucas <lucassantosamaral@hotmail.com>
2017-09-24 14:36:28 -0300

    Exportando mais de um componente

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default props => (
-     <h1>{props.value}</h1>
+//Uso de parenteses para componentes mais extensos
+export  default props =>(
+    <h1>Primeiro Componente!</h1>
 )
+
+//Um único componente
+export const Segundo = props => <h1>Segundo Componente!</h1>
+
+//export { Primeiro, Segundo }
